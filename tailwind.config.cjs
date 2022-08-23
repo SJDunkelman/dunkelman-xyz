c7cf95a17003247250df275750ea04cba07d3bc2
Simon Dunkelman <sd9518@ic.ac.uk>
2022-08-23 21:59:14 +0100

    first commit 🚀

---
+++ tailwind.config.cjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	options: {
+		safelist: ["bg-orange"],
+	},
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				'light-grey': '#444444',
+				'grey': '#222222',
+				'cream': '#FBF5F3',
+				'light-purple': '#CDCEE3',
+				'purple': '#B4ADEA',
+				'orange': '#EBA133',
+				'light-teal': '#C3DFE0',
+				'blue': '#D0F0FC',
+				'deep-blue': '#004FFE',
+				'green': '#7FC29B',
+				'muted-green': '#B6C5C4',
+			},
+		},
+	},
+	plugins: [],
+}
