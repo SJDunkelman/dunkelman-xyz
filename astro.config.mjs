6142895b68dee09924ec06210f62cded2d75e522
simondunkelman <simon@dunkelman.xyz>
2024-07-16 22:03:21 +0100

    added netlify integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
-	  applyBaseStyles: false,
-  }), react()]
+    applyBaseStyles: false
+  }), react()],
+  output: "server",
+  adapter: netlify()
 });
